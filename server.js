8dd39c8a4c4ca0328702fbb55b6d16062aed8c97
Erika <erikarp28@hotmail.com>
2021-11-17 20:31:54 -0600

    Update server with routes to request header parser

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/timestamp", function (req, res) {
+  res.sendFile(__dirname + '/views/timestamp.html');
+});
+
+app.get("/requestHeader", function (req, res) {
+  res.sendFile(__dirname + '/views/requestHeader.html');
+});
+
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
