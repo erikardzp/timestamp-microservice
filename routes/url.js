bc5b8252d4178b60d6050ccfc76b0f1ab2ba9b63
Erika <erikarp28@hotmail.com>
2021-11-18 21:16:29 -0600

    Update url.js to know if a url is valid or not

--- routes/url.js
+++ routes/url.js
@@ -21,58 +21,53 @@ router.use(bodyParser.json());
 // The API base Url endpoint
 const baseUrl = 'http:localhost:3000'
 
-router.post('/shorturl', async (req, res) => {
+router.post('/shorturl', (req, res) => {
 
-    // console.log(req.body);
     const {
         url
     } = req.body // destructure the longUrl from req.body.longUrl
 
-    // check base url if valid using the validUrl.isUri method
-    if (!validUrl.isUri(url)) {
-        return res.status(401).json({error: 'invalid url'})
-    }
-
-    // if valid, we create the url code
-    const urlCode = shortid.generate()
-
-    // check long url if valid using the validUrl.isUri method
-    if (validUrl.isUri(url)) {
-        try {
-            /* The findOne() provides a match to only the subset of the documents 
-            in the collection that match the query. In this case, before creating the short URL,
-            we check if the long URL was in the DB ,else we create it.
-            */
-            let url2 = await Url.findOne({
-                url
-            })
-
-            // url exist and return the respose
-            if (url2) {
-                res.json({"original_url": url2.url, "short_url": url2.urlCode})
-            } else {
-                // join the generated short code the the base url
-                const shortUrl = urlCode
-
-                // invoking the Url model and saving to the DB
-                url2 = new Url({
-                    url,
-                    shortUrl,
-                    urlCode,
-                    date: new Date()
+    // check if url is valid using urlExists method
+    urlExists(url, async function(err, exists) {
+        // If valid, we create the url code
+        const urlCode = shortid.generate()
+        if(exists){
+            try {
+                /* The findOne() provides a match to only the subset of the documents 
+                in the collection that match the query. In this case, before creating the short URL,
+                we check if the long URL was in the DB ,else we create it.
+                */
+                let url2 = await Url.findOne({
+                    url
                 })
-                await url2.save()
-                res.json({"original_url": url2.url, "short_url": url2.urlCode})
+    
+                // url exist and return the respose
+                if (url2) {
+                    res.json({"original_url": url2.url, "short_url": url2.urlCode})
+                } else {
+                    // join the generated short code the the base url
+                    const shortUrl = urlCode
+    
+                    // invoking the Url model and saving to the DB
+                    url2 = new Url({
+                        url,
+                        shortUrl,
+                        urlCode,
+                        date: new Date()
+                    })
+                    await url2.save()
+                    res.json({"original_url": url2.url, "short_url": url2.urlCode})
+                }
             }
+            // exception handler
+            catch (err) {
+                console.log(err)
+                res.status(500).json('Server Error')
+            }
+        }else{
+            res.status(401).json({error: 'invalid url'})
         }
-        // exception handler
-        catch (err) {
-            console.log(err)
-            res.status(500).json('Server Error')
-        }
-    } else {
-        res.status(401).json({error: 'invalid url'})
-    }
+    });
 })
 
 module.exports = router
